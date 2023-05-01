ee2ec52b8448c4e72e33cd212c34dbb03e9f96b0
YuliaRomaniv <yulia-romaniv@ukr.net>
2023-05-02 02:17:57 +0300

    hm_js_1

---
+++ hm_js_1/hm_1.js
@@ -0,0 +1,49 @@
+let i='hello'
+console.log(i)
+let p = 'owu'
+console.log(p)
+let r = 'com'
+console.log(r)
+let s = 'ua'
+console.log(s)
+let ab = 1
+console.log(ab)
+let bc = 10
+console.log(bc)
+let cd = -999
+console.log(cd)
+let d = 123
+console.log(d)
+let f = 3.14
+console.log(f)
+ f = 2.7
+console.log(f)
+ f = 16
+console.log(f)
+
+let check = true
+let check2 = false
+console.log(check)
+console.log(check2)
+
+let firstName = "Romaniv"
+let middleName = 'Yulia'
+let lastName = 'Oleksandrovna'
+let person = `${firstName} ${middleName} ${lastName}`
+console.log(person);
+
+
+let a = 100
+console.log(typeof a)
+let b = '100'
+console.log(typeof b)
+let c = true
+console.log(typeof c)
+
+let name = prompt()
+console.log(name)
+
+
+
+
+
